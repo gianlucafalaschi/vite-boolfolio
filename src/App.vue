b5d22c74d25104e696353861a4619d9021eac5ff
gianlucafalaschi <gian100gian@outlook.it>
2024-06-10 18:59:30 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<script>
+export default {
+
+}
+</script>
+
+<template>
+  <h1>Ciao Ciao</h1>
+</template>
+
+<style lang="scss">
+@use './style/generic';
+@use './style/partials/variables' as *;  // le variables vanno importate in tutti ii componenti figli se vengono usate
+
+h1 {
+  color: $main-color;
+}
+</style>
