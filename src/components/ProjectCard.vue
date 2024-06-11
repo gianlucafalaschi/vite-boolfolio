14c3161530554cd7af0df470b38581ae467c1a43
gianlucafalaschi <gian100gian@outlook.it>
2024-06-11 17:10:50 +0200

    add function to truncate text in summary

--- src/components/ProjectCard.vue
+++ src/components/ProjectCard.vue
@@ -10,6 +10,19 @@ export default {
       
     }
   },
+  methods: {
+    truncateText(text) {
+      // se il testo è piu lungo di 100 caratteri
+      // lo taglio a 100 caratteri e aggiungo ...
+      if(text.length > 100) {
+       return text.substr(0, 99) + '...'  // substr torna una funzione di testo, parte dal carattere 0 fino al 99
+      }
+      // altrimenti il testo rimane invariato
+      return text   
+      
+    }
+  },
+  
 }
 </script>
 
@@ -22,11 +35,12 @@ export default {
                 <div v-if="projectDetails.type">
                     <strong>Type:</strong> {{ projectDetails.type.name  }}
                 </div>
+                <!-- stampo le technologies solo se l'array che le contiene ha piu' di 0 elementi -->
                 <div v-if="projectDetails.technologies.length > 0">
                     <strong>Technology:</strong> <span v-for="technology in projectDetails.technologies"> {{ technology.name }} &nbsp </span>
                 </div>
-                <!-- stampo il summary solo se è popolato --> 
-                <p v-if="projectDetails.summary"  class="card-text">{{ projectDetails.summary }}</p>
+                <!-- stampo il summary solo se è popolato -->          <!-- chiamo la funzione che taglia il testo se sopra i 100 caratteri -->
+                <p v-if="projectDetails.summary"  class="card-text">{{ truncateText(projectDetails.summary) }}</p>
                  <!--  <a href="#" class="btn btn-primary">Go somewhere</a> -->
             </div>
         </div>
